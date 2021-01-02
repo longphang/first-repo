f0c07dcba73cb0c234bb4e5afef6228c6cac1f14
Long Phang <longphang@hotmail.com>
2021-01-02 20:13:14 +0000

    add new read file function

--- my_python.py
+++ my_python.py
@@ -2,11 +2,15 @@ import os
 import sys
 
 
+def read_file(file):
+    with open(file, 'r') as f:
+        data = f.read()
+    return data
+
+
 def main():
     print('Hello World - this is on branch test1')
 
 
 if __name__ == '__main__':
     main()
-
-
