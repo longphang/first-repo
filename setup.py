ab97db73db0c8020fb70567bcef546f8c2a5bc21
Long Phang <longphang@hotmail.com>
2023-04-07 21:20:31 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='lplib',
+    version='1.0.0',
+    author='LP',
+    author_email='test@sample.com',
+    description='Test library',
+    packages=find_packages(),
+)
