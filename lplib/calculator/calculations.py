b3fa9d5ed4961064e75536491c2a37956c5310a8
Long Phang <longphang@hotmail.com>
2023-04-07 21:20:11 +0100

    add package

---
+++ lplib/calculator/calculations.py
@@ -0,0 +1,15 @@
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+def minus(x: int, y: int) -> int:
+    return x - y
+
+
+def multiply(x: int, y: int) -> int:
+    return x * y
+
+
+def divide(x: int, y: int) -> int:
+    return x // y
